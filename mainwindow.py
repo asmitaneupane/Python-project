88bb478ac0effc1835645cc728eef4bb0627e048
asmita <neupane.ashmita2056@gmail.com>
2019-04-01 13:11:42 +0545

    buttonreply

--- mainwindow.py
+++ mainwindow.py
@@ -1,9 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QComboBox, QDialog, QDialogButtonBox, QFormLayout, QGridLayout, QGroupBox, QHBoxLayout,QLabel, QLineEdit, QMenu, QMenuBar, QPushButton, QSpinBox, QTextEdit,QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QMessageBox
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtGui import QIcon
-
-import sys
  
 class App(QMainWindow):
  
@@ -22,8 +20,13 @@ class App(QMainWindow):
       self.setGeometry(self.left, self.top, self.width, self.height)
       self.statusBar().showMessage('Message in statusbar.')
       button = QPushButton('Click', self)
-      
+      button.clicked.connect(self.on_click)
       button.setToolTip('This is an example button')
+      buttonReply = QMessageBox.question(self, 'PyQt5 message', "Do you like PyQt5?", QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+      if buttonReply == QMessageBox.Yes :
+         print('Yes clicked.')
+      else :
+         print('No clicked.')
       button.move(100,70)
       self.show()
 
